8b058fd9ae2b0822f15a27dbc71d426dc7f754bd
maarlesh <subamaarleshbaskar@gmail.com>
2026-04-05 15:08:53 +0530

    Added delete account functionality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.seriesly"
+        applicationId = "com.maarlesh.seriesly"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
